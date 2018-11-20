e5f9791d6ef8a10721374595483736dee4f282b0
RAKPONG THORHUN <rakpong_thorhun@cmu.ac.th>
2018-11-20 13:41:27 +0700

    mvc calculate

---
+++ CPE200Lab1/CPE200Lab1/controller.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPE200Lab1
+{
+	public class controller
+	{
+		CalculatorEngine engine1;
+		RPNCalculatorEngine engine2;
+		public controller()
+		{
+			engine1 = new CalculatorEngine();
+			engine2 = new RPNCalculatorEngine();
+		}
+
+		public string Calculate(string str)
+		{
+			return engine1.calculate(str);
+		}
+
+		public string RPNcalculate(string str)
+		{
+			return engine2.calculate(str);
+		} 
+
+	}
+}
